270ca9ff0d9605e75af68729370465f8fa927d8f
Vladarbinyan <clab@mail.ru>
2021-04-17 00:06:33 +0700

    fixes

--- run_demoserver.py
+++ run_demoserver.py
@@ -4,27 +4,6 @@ from uBricks.framework import WebFramework
 from demo_routes import routes
 
 
-<<<<<<< HEAD
-
-# A relatively simple WSGI application. It's going to print out the
-# environment dictionary after being updated by setup_testing_defaults
-
-# def simple_app(environ, start_response):
-#     setup_testing_defaults(environ)
-#
-#     status = '200 OK'
-#     headers = [('Content-type', 'text/plain; charset=utf-8')]
-#
-#     start_response(status, headers)
-#
-#     ret = [("%s: %s\n" % (key, value)).encode("utf-8")
-#            for key, value in environ.items()]
-#     return ret
-
-# Front controllers
-
-=======
->>>>>>> step1
 def secret_front(request):
     request['secret'] = 'some secret'
 
